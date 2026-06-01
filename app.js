cac4702d5055c58d26cb2723178388593cb843a8
Sujan Aryal <sujan@kingscollege.edu.np>
2026-06-01 21:53:57 +0545

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const API_URL = "https://script.google.com/macros/s/AKfycbxJOVEJ1NNGu7KUjRJn8Pqmb32Qg_cmp2p68RPi6zhE5rAKdSMAwKxjQg6FSI0WC6Sr/exec"; 
+const API_URL = "https://script.google.com/macros/s/AKfycbz2whhW64YA38EzqdePpJHbGf8DnbaJJi1zsASkLcLn8wj09IFpLAnSbSxNYgrHeoBl/exec"; 
 
 const html5QrCode = new Html5Qrcode("reader");
 
